efdb356cfc4bc785e589c0f0921ef4d7fd12e21e
Ose Ughu <oseughu@gmail.com>
2022-09-21 14:22:04 +0100

    add update user mutation

---
+++ app/graphql/mutations/edit_user.rb
@@ -0,0 +1,28 @@
+class Mutations::CreateSub < Mutations::BaseMutation
+  argument :name, String, required: true
+  argument :email, String, required: true
+  argument :city, String, required: true
+
+  field :user, Types::UserType, null: false
+  field :message, String, null: false
+  field :errors, [String], null: false
+
+  def resolve(name:, email:, city:)
+    user = User.find_by(email:)
+
+    if user
+      updated_user = user.update(name:, email:, city:)
+      {
+        user: updated_user,
+        message: "User profile updated for #{email}.",
+        errors: []
+      }
+    else
+      {
+        user: nil,
+        message: 'User not found.',
+        errors: user.errors.full_messages
+      }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_sub, mutation: Mutations::CreateSub
     field :delete_sub, mutation: Mutations::DeleteSub
+    field :edit_user, mutation: Mutations::EditUser
   end
 end
